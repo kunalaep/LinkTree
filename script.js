50495e35e41b7de35c3040f1d898c2f75c0bd83b
Kunal Kumar <105972421+kunalaep@users.noreply.github.com>
2023-04-23 21:50:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Engineer","Developer", "Video Editor", "Designer", "Freelancer","Editor Guy"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
